1c6fb7a0cfd672ec59f2abac94c973fb5cbfc2be
Jaimit Patel <jaimit_12@hotmail.com>
2022-04-03 16:38:39 -0400

    added idb functionality

---
+++ public/js/idb.js
@@ -0,0 +1,63 @@
+let db;
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('budget', { autoIncrement: true });
+  };
+
+request.onsuccess = function(event) {
+    db = event.target.result;
+  
+    if (navigator.onLine) {
+            uploadBudget();
+
+    }
+  };
+  
+  request.onerror = function(event) {
+        console.log(event.target.errorCode);
+  };
+
+function saveRecord(record) {
+    const transaction = db.transaction(['budget'], 'readwrite');  
+    const budgetObjectStore = transaction.objectStore('budget');  
+    budgetObjectStore.add(record);
+  }
+
+  function uploadBudget() {
+    const transaction = db.transaction(['budget'], 'readwrite');
+  
+    const budgetObjectStore = transaction.objectStore('budget');
+  
+    const getAll = budgetObjectStore.getAll();
+  
+  }
+
+getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(['budget'], 'readwrite');
+          const budgetObjectStore = transaction.objectStore('budget');
+          budgetObjectStore.clear();
+
+          alert('All saved budget changes have been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+  window.addEventListener('online', uploadBudget);
